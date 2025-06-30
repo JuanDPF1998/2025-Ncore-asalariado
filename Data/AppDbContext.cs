7b041c2d26225f315676754c4a5be7519d7c7d03
Juan DPF1998 <JuanDanielPF1998@gmail.com>
2025-06-30 15:25:50 -0600

    tercer commit: DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _2025_NetCore_Empleados.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<AppDbContext> empleadosDbSet { get; set; }
+    }
+}
